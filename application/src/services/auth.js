b7d9d95221d306dacd33ae3accaa7c069f5d4ac3
Firmino Changani <firmino.changani@jungle.ai>
2019-10-12 16:05:20 +0100

    Bootstrap auth service

---
+++ application/src/services/auth.js
@@ -0,0 +1,8 @@
+/**
+ * auth
+ * @params {string} email
+ * @params {string} password
+ *
+ * @returns {Promise} An object that represents auth failure or success
+ */
+module.exports.auth = (email, password) => {};
