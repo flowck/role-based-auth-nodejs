862464983e0bdc20706bd89ddcbe73d966f2179a
Firmino Changani <firmino.changani@jungle.ai>
2019-10-13 18:22:01 +0100

    Prettier

--- application/src/data/postgres/migrations/20191005205840-create-users.js
+++ application/src/data/postgres/migrations/20191005205840-create-users.js
@@ -10,7 +10,7 @@ module.exports = {
       },
       name: {
         type: Sequelize.STRING,
-        allowNull: false,
+        allowNull: false
       },
       email: {
         type: Sequelize.STRING,
@@ -19,7 +19,7 @@ module.exports = {
       },
       password: {
         type: Sequelize.STRING,
-        allowNull: false,
+        allowNull: false
       },
       createdAt: {
         allowNull: false,
